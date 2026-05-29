7dac71c7a30f1c394ea7a8e1d6f54ff026c7b21c
Kelwin Karan <kevkaran@proton.me>
2026-05-29 20:24:52 -0300

    Refactor main.c to define Player struct and streamline drawing logic

--- main.c
+++ main.c
@@ -1,19 +1,51 @@
 #include "raylib.h"
+#include <math.h>
+
+typedef struct {
+    Vector2 position;
+    float rotation;
+    float size;
+    Color color;
+} Player;
+
+const int screenWidth = 800;
+const int screenHeight = 600;
 
 int main(void)
 {
-    const int screenWidth = 800;
-    const int screenHeight = 600;
-
     InitWindow(screenWidth, screenHeight, "AstroKinetics - BolaFora");
+
+    Player ship = { 0 };
+    ship.position = (Vector2) { screenWidth / 2.0f, screenHeight / 2.0f };
+    ship.rotation = 0.0f;
+    ship.size = 20.0f;
+    ship.color = RAYWHITE;
+
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(BLACK);
 
-        DrawText("AstroKinetics TESTE pew pew!", 250, 280, 20, GREEN);
-        DrawFPS(10, 10);
+        float baseAngle = ship.rotation - 90.0f;
+
+        Vector2 p1 = {
+            ship.position.x + cosf(baseAngle * DEG2RAD) * ship.size,
+            ship.position.y + sinf(baseAngle * DEG2RAD) * ship.size
+        };
+
+        Vector2 p2 = {
+            ship.position.x + cosf((baseAngle + 140.0f) * DEG2RAD) * ship.size,
+            ship.position.y + sinf((baseAngle + 140.0f) * DEG2RAD) * ship.size
+        };
+
+        Vector2 p3 = {
+            ship.position.x + cosf((baseAngle - 140.0f) * DEG2RAD) * ship.size,
+            ship.position.y + sinf((baseAngle - 140.0f) * DEG2RAD) * ship.size
+        };
+
+        DrawTriangleLines(p1, p2, p3, ship.color);
+
         EndDrawing();
     }
 
